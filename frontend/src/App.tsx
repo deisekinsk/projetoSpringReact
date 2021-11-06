ee2185f1cf1c2482cdf22b30c581041ec32de881
deisesilva <kinsk@msn.com>
2021-11-05 23:47:08 -0300

    Style.css in Assets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 function App() {
   return (
-    <div>
+    <div >
       <h1>| Hello 🧼Clean Project🧼 of Spring.React |</h1>
       <h1>I'm Deise Kinsk 👩🏽‍💻.</h1>
-      <h1>I lookin for a oportunity of a developer entry-level job.</h1>      
+      <h1>I am looking for an opportunity for a developer entry-level job.</h1>      
     </div>
   );
 }

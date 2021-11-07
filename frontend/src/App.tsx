ad6e3f14c4f30f451e7d805def78afffa4630a08
deisesilva <kinsk@msn.com>
2021-11-07 11:27:17 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import NavBar from "components/NavBar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
 
 function App() {
   return (
     <>
       <NavBar />
       <div  className="container">
-        <h1>   | Hi | </h1>
-        <h1 className="text-primary">I'm Deise Kinsk 👩🏽‍💻.</h1>
+        <h1> Hi</h1>
+        <h1 className="text-primary">I'm Deise Kinsk. </h1>
         <h1>I am looking for an opportunity for a developer entry-level job.</h1>
+        <DataTable />
       </div>
+      
+      <Footer />
     </>
   );
 }
